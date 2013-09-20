33b714a4b4158f94a1f12623958849d3c74135a7
josevokt <joseph.vokt@gmail.com>
2013-09-20 10:49:56 -0400

    Setup

---
+++ writeup/auto/pset2.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "pset2"
+ (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     ""
+     "article"
+     "art10"
+     "fancyhdr"
+     "amsmath"
+     "enumerate"
+     "graphicx"
+     "caption"
+     "subcaption")))
+
