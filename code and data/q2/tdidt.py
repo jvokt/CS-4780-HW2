879d2025591bec6abbd8d0bd768a34a23bef7438
Trevor Martin Slaton <tms245@cornell.edu>
2013-09-21 21:57:11 -0400

    closer but still need to implement info gain

--- code and data/q2/tdidt.py
+++ code and data/q2/tdidt.py
@@ -27,26 +27,32 @@ def growthDataFromFile(data_file):
         for feature_id in example:
             if feature_id not in count:
                 count[feature_id] = {}
-            if count[feature_id][feature_value] not in count[feature_id]:
+            feature_value = int(round(example[feature_id],0)) # split on ints   
+            if feature_value not in count[feature_id]:
                 count[feature_id][feature_value] = defaultdict(int)
-            feature_value = example[feature_id]
             count[feature_id][feature_value][label] += 1
     f.close()
     return count
     
 def examplesFromFile(data_file):
-    examples = defaultdict([])
+    examples = defaultdict(list)
     f = open(data_file, 'r')
     for line in f:
         label, example = parseLine(line)
         examples[label].append(example)
     f.close()
-   return examples
+    return examples
 
-def info(n,p):
+def entropy(n,p):
     frac_p = p/float(p+n)
     frac_n = n/float(p+n)
+    print p, n
     return -frac_p*math.log(frac_p,2) - frac_n*math.log(frac_n,2)
+    
+def infoGain(n,p):
+    return None
+    # TODO: figure this out
+    # see http://stackoverflow.com/questions/1859554/what-is-entropy-and-information-gain
     
 class TDIDT:
     def __init__(self, data_file, y_def=0):
@@ -61,11 +67,11 @@ class TDIDT:
     
         examples = examplesFromFile(data_file)
         count = growthDataFromFile(data_file)
-        self.grow(self, examples, count)
+        self.grow(examples, count)
     
     def classify(self, example):
         if self.label is None:
-            if self.splitCriterion(example)
+            if self.splitCriterion(example):
                 return self.yes.classify(example)
             else:
                 return self.no.classify(example)
@@ -73,31 +79,37 @@ class TDIDT:
             return self.label
     
     def grow(self, examples, count, y_def=0):
-        if examples.keys() == []:
+        labels = examples.keys()
+        if labels == []:
             self.label = y_def
+        elif len(labels) == 1:
+            self.label = labels[0]
         else:
-            first_label = examples.keys()[0]
-            identical = True
-            for label in examples:
-                indentical &= label == first_label
-            if identical:
-                self.label = first_label
-            else:
-                max_info = -sys.maxint-1
-                for feature_id in count:
-                    for feature_value in count[feature_id]:
-                        n = count[feature_id][feature_value][0]
-                        p = count[feature_id][feature_value][1]
-                        i = info(n,p)
-                        if i > max_info:
-                            max_info = i
-                            attr, thresh = feature_id, feature_value
-                def splitCriterion(example):
+            max_gain = -float('inf')
+            attr = thresh = None
+            for feature_id in count:
+                for feature_value in count[feature_id]:
+                    n = count[feature_id][feature_value][0]
+                    p = count[feature_id][feature_value][1]
+                    g = infoGain(n,p)
+                    if g > max_gain:
+                        max_gain = g
+                        attr, thresh = feature_id, feature_value
+            def splitCriterion(example):
+                if attr is not None and thresh is not None:
                     return example[attr] >= thresh
-                self.splitCriterion = splitCriterion
-                del count[feature_id]
-                self.yes = self.grow(self, examples, count, y_def=0)
-                self.no = self.grow(self, examples, count, y_def=0)
+            self.splitCriterion = splitCriterion
+            if attr is not None:
+                del count[attr]
+                yeses, nos = defaultdict(list), defaultdict(list)
+                for label in examples:
+                    example = examples[label]
+                    if self.splitCriterion(example):
+                        yeses[label].append(example)
+                    else:
+                        nos[label].append(example)
+                self.yes = self.grow(yeses, count, y_def)
+                self.no = self.grow(nos, count, y_def)
  
 def main(data_file):
     tdidt = TDIDT(data_file)
